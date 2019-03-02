a2130397c5a3fc093818192701dc9f584b80a554
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2019-03-01 17:01:43 -0800

    First commit

---
+++ simple-store/src/components/ShowGreetingsComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: mapGetters({
+        msg: 'getMessage'
+    })
+  }
+</script>
---
+++ simple-store/src/vuex/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+const state = {
+  message: 'Hello Vue!'
+}
+ 
+const mutations = {
+  changeMessage(state, msg) {
+    state.message = msg
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters
+})
